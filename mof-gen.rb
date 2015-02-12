24e2db087af5032a2b63f37813afd9e09cdfe281
louisscruz <louisstephancruz@me.com>
2015-02-12 13:07:14 -0500

    Rewrite comments

--- mof-gen.rb
+++ mof-gen.rb
@@ -1,3 +1,9 @@
+# This script generates Multiple-Order-Function (MOF) rows according to Babbitt's proposed method:
+# i^n mod p, where p is prime and i is a primitive root of p
+# With this formula, generate a series through progressive squaring, from zero to (p-1).
+# Collect the residue, mod p and perform an x-swap with each object's index number.
+#
+
 def mof(i, p)
   arr = []
   x_swap = []
@@ -10,4 +16,17 @@ def mof(i, p)
   return x_swap
 end
 
-puts mof(5, 23)
+def mof_nat(i, p)
+  arr = []
+  x_swap = []
+  (0..(p-2)).each do |x|
+    arr.push((i**x) % p)
+  end
+  (1..(p-1)).each do |n|
+    x_swap.push((arr.index {|item| n == item }))
+  end
+  return x_swap
+end
+
+puts mof(2, 19)     # This method will produce the MOF series for 2^n mod 19 mapped to 12 pitches.
+puts mof_nat(2, 19) # This method will produce the MOF series for 2^n mod 19 without mapping to 12 pitches.
